e6adbb47454f64d5a38d6ec36db8cb6d90fc6fdf
zamay2024 <79635300+zamay2024@users.noreply.github.com>
2022-01-15 17:56:12 +0300

    Add files via upload

---
+++ Kursovaya_MK/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace Kursovaya_MK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.tbHumidity = new System.Windows.Forms.TextBox();
+            this.tbTemperature = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbBaudRate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnPortList = new System.Windows.Forms.Button();
+            this.tbSlave = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbPorts = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.btnUpdate);
+            this.groupBox1.Controls.Add(this.tbHumidity);
+            this.groupBox1.Controls.Add(this.tbTemperature);
+            this.groupBox1.Location = new System.Drawing.Point(12, 180);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 160);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Данные";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Температура";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Влажность";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(280, 60);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(140, 35);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Обновить данные\r\n";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // tbHumidity
+            // 
+            this.tbHumidity.Location = new System.Drawing.Point(82, 97);
+            this.tbHumidity.Name = "tbHumidity";
+            this.tbHumidity.Size = new System.Drawing.Size(113, 20);
+            this.tbHumidity.TabIndex = 3;
+            this.tbHumidity.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbTemperature
+            // 
+            this.tbTemperature.Location = new System.Drawing.Point(82, 46);
+            this.tbTemperature.Name = "tbTemperature";
+            this.tbTemperature.Size = new System.Drawing.Size(113, 20);
+            this.tbTemperature.TabIndex = 2;
+            this.tbTemperature.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox2.Controls.Add(this.tbBaudRate);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.btnPortList);
+            this.groupBox2.Controls.Add(this.tbSlave);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.cbPorts);
+            this.groupBox2.Controls.Add(this.btnConnect);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(460, 160);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Соединение";
+            // 
+            // tbBaudRate
+            // 
+            this.tbBaudRate.Location = new System.Drawing.Point(82, 68);
+            this.tbBaudRate.Name = "tbBaudRate";
+            this.tbBaudRate.Size = new System.Drawing.Size(113, 20);
+            this.tbBaudRate.TabIndex = 10;
+            this.tbBaudRate.Text = "9600";
+            this.tbBaudRate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Slave";
+            // 
+            // btnPortList
+            // 
+            this.btnPortList.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPortList.Location = new System.Drawing.Point(280, 26);
+            this.btnPortList.Name = "btnPortList";
+            this.btnPortList.Size = new System.Drawing.Size(140, 35);
+            this.btnPortList.TabIndex = 8;
+            this.btnPortList.Text = "Обновить список портов";
+            this.btnPortList.UseVisualStyleBackColor = true;
+            this.btnPortList.Click += new System.EventHandler(this.btnPortList_Click);
+            // 
+            // tbSlave
+            // 
+            this.tbSlave.Location = new System.Drawing.Point(82, 110);
+            this.tbSlave.Name = "tbSlave";
+            this.tbSlave.Size = new System.Drawing.Size(113, 20);
+            this.tbSlave.TabIndex = 1;
+            this.tbSlave.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Baud Rate";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port";
+            // 
+            // cbPorts
+            // 
+            this.cbPorts.FormattingEnabled = true;
+            this.cbPorts.Location = new System.Drawing.Point(82, 23);
+            this.cbPorts.Name = "cbPorts";
+            this.cbPorts.Size = new System.Drawing.Size(113, 21);
+            this.cbPorts.TabIndex = 3;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(280, 79);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(140, 35);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Подключить";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(484, 351);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Мониторинг";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbTemperature;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbPorts;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox tbHumidity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbSlave;
+        private System.Windows.Forms.Button btnPortList;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbBaudRate;
+    }
+}
+
---
+++ Kursovaya_MK/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Kursovaya_MK
+{
+    public partial class Form1 : Form
+    {
+        string dataIN;
+        SerialPort serialPort1 = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (btnConnect.Text == "Подключить")
+            {
+                try
+                {
+                    serialPort1.PortName = cbPorts.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(tbBaudRate.Text);
+                    serialPort1.Open();
+                    Thread.Sleep(1000);
+
+                    cbPorts.Enabled = false;
+                    
+                    btnConnect.Text = "Отключить";
+
+                    dataIN = serialPort1.ReadExisting();
+                   
+                    char[] dataArr = dataIN.ToCharArray();
+                    char i = Convert.ToChar(tbSlave.Text);
+                    if (i == dataArr[dataArr.Length - 5])
+                     {
+                    
+                    
+                        tbTemperature.Text = $"{dataArr[dataArr.Length - 4]}{dataArr[dataArr.Length - 3]}";
+                        tbHumidity.Text = $"{dataArr[dataArr.Length - 2]}{dataArr[dataArr.Length - 1]}";
+                  }
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message, "Ошибка подключения", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                serialPort1.Close();
+                cbPorts.Enabled = true;
+                
+                btnConnect.Text = "Подключить";
+            }     
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            dataIN = serialPort1.ReadExisting();
+           
+            char[] dataArr = dataIN.ToCharArray();
+            char i = Convert.ToChar(tbSlave.Text);
+            if (i == dataArr[dataArr.Length - 5])
+            {
+                tbTemperature.Text = $"{dataArr[dataArr.Length - 4]}{dataArr[dataArr.Length - 3]}";
+                tbHumidity.Text = $"{dataArr[dataArr.Length - 2]}{dataArr[dataArr.Length - 1]}";
+            }
+            else
+               MessageBox.Show("Slave недоступен.", "Ошибка");
+        }
+
+        private void btnPortList_Click(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cbPorts.Text = "";
+            cbPorts.Items.Clear();
+            if (ports.Length != 0)
+            {
+                cbPorts.Items.AddRange(ports);
+                cbPorts.SelectedIndex = 0;
+            }
+            else
+                MessageBox.Show("Нет доступных портов.","Ошибка");
+        }
+    }
+}
